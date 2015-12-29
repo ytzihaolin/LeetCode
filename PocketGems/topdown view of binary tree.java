46a927e3a02b8b9f7f64104376cdf458e4680861
ytzihaolin <ytzihalin@gmaill.com>
2015-12-29 00:52:55 -0500

    d

--- PocketGems/topdown view of binary tree.java
+++ PocketGems/topdown view of binary tree.java
@@ -92,3 +92,75 @@ public class TopDownViewofBinaryTree {
         t.printTopView();
     }
 }
+
+
+
+
+bottom up view use treemap to store horizontal index and treenode;
+
+
+public void bottomView()
+    {
+        if (root == null)
+            return;
+ 
+        // Initialize a variable 'hd' with 0 for the root element.
+        int hd = 0;
+ 
+        // TreeMap which stores key value pair sorted on key value
+        Map<Integer, Integer> map = new TreeMap<>();
+ 
+         // Queue to store tree nodes in level order traversal
+        Queue<TreeNode> queue = new LinkedList<TreeNode>();
+ 
+        // Assign initialized horizontal distance value to root
+        // node and add it to the queue.
+        root.hd = hd;
+        queue.add(root);
+ 
+        // Loop until the queue is empty (standard level order loop)
+        while (!queue.isEmpty())
+        {
+            TreeNode temp = queue.remove();
+ 
+            // Extract the horizontal distance value from the
+            // dequeued tree node.
+            hd = temp.hd;
+ 
+            // Put the dequeued tree node to TreeMap having key
+            // as horizontal distance. Every time we find a node
+            // having same horizontal distance we need to replace
+            // the data in the map.
+            map.put(hd, temp.data);
+ 
+            // If the dequeued node has a left child add it to the
+            // queue with a horizontal distance hd-1.
+            if (temp.left != null)
+            {
+                temp.left.hd = hd-1;
+                queue.add(temp.left);
+            }
+            // If the dequeued node has a left child add it to the
+            // queue with a horizontal distance hd+1.
+            if (temp.right != null)
+            {
+                temp.right.hd = hd+1;
+                queue.add(temp.right);
+            }
+        }
+ 
+        // Extract the entries of map into a set to traverse
+        // an iterator over that.
+        Set<Entry<Integer, Integer>> set = map.entrySet();
+ 
+        // Make an iterator
+        Iterator<Entry<Integer, Integer>> iterator = set.iterator();
+ 
+        // Traverse the map elements using the iterator.
+        while (iterator.hasNext())
+        {
+            Map.Entry<Integer, Integer> me = iterator.next();
+            System.out.print(me.getValue()+" ");
+        }
+    }
+}
