8872f444b4ffc7769f521e99cc3481769724ed0d
ytzihaolin <ytzihalin@gmaill.com>
2016-01-18 15:31:43 -0800

    up

--- amazon/Gray code.java
+++ amazon/Gray code.java
@@ -8,3 +8,15 @@ public class Solution {
         return result;
     }
 }
+
+
+public static int grayCheck(byte term1, byte term2){
+
+	byte x=(byte)(term1^term2);
+	int count=0;
+	while(x!=0){
+		x=(byte)(x&(x-1));
+		count++;
+	}
+	return count==1?1:0;
+}
