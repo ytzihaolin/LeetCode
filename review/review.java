ce0d4da3e912acd74ea8618ff32cb801c44687ea
ytzihaolin <ytzihalin@gmaill.com>
2016-01-25 23:48:25 -0800

    up

--- review/review.java
+++ review/review.java
@@ -345,3 +345,294 @@ multiplytemp=1.val 当当前数字位前为+号
 currentvalue=1. currentvalue+val
 		     2. currentvalue-val
 		     3. currentvalue-multiplytemp+multiplytemp*val//此处为关键
+
+
+
+
+
+124. Binary Tree Maximum Path Sum 
+Given a binary tree, find the maximum path sum.
+
+For this problem, a path is defined as any sequence of nodes from some starting node to any node in the tree along the parent-child connections. The path does not need to go through the root.
+
+使用递归求单边path sum非常简单，此处只要加一个全局变量， 每次就sum=max{sum,maxleft+maxright+root.val}, 递归返回值为单边max即可
+
+
+84. Largest Rectangle in Histogram 
+Given n non-negative integers representing the histograms bar height where the width of each bar is 1, find the area of largest rectangle in the histogram.
+
+O(n) stack based solution
+遍历height array， stack中存递增高度，保证每一次插入的元素都比stack顶元素height要高，否则持续pop stack顶元素
+意味着pop出的高度作为长方形左边找到了最右边的边（当前遍历指针指向的元素）， 高度按每次pop出的高度算，因为可以肯定在当前指针之前的高度均高于pop出的元素，否则不会存在在stack中
+。计算这个长方形的面积并维持最大值。记住结束遍历后别忘了继续pop元素计算面积。
+
+
+
+85. Maximal Rectangle 
+Given a 2D binary matrix filled with 0's and 1's, find the largest rectangle containing all ones and return its area.
+
+紧接上面的问题，每次遍历每行更新height数组，然后用相同的方法求最大长方形面积，记录全局最大即可。
+
+
+41. First Missing Positive 
+Given an unsorted integer array, find the first missing positive integer.
+
+For example,
+Given [1,2,0] return 3,
+and [3,4,-1,1] return 2.
+
+Your algorithm should run in O(n) time and uses constant space.
+
+利用原数组index来表征，若元素为正则移动到index=当前元素值得位置（注意如果移动位置的元素已经是num[i]=i，那就不要移动防止loop）。
+最后遍历看哪一个元素不等于index即可。
+
+
+
+56. Merge Intervals 
+Given a collection of intervals, merge all overlapping intervals.
+
+For example,
+Given [1,3],[2,6],[8,10],[15,18],
+return [1,6],[8,10],[15,18].
+
+首先写comparator以interval start为标准排序，然后遍历排好序的list，注意维持一个start， end变量表示当前正在遍历的interval的起始，
+如果新遍历到的元素start小于end那么更新start，end而非插入res，直到新元素start>当前记录的end则插入res。
+
+
+
+
+57. Insert Interval 
+Given a set of non-overlapping intervals, insert a new interval into the intervals (merge if necessary).
+
+You may assume that the intervals were initially sorted according to their start times.
+
+Example 1:
+Given intervals [1,3],[6,9], insert and merge [2,5] in as [1,5],[6,9].
+
+Example 2:
+Given [1,2],[3,5],[6,7],[8,10],[12,16], insert and merge [4,9] in as [1,2],[3,10],[12,16].
+
+This is because the new interval [4,9] overlaps with [3,5],[6,7],[8,10].
+
+1. 用和上一题一样的方法，先插入，后merge。
+2. 将上一题中merge的逻辑单独提出来，写插入新interval的部分。
+	while (i < intervals.size() && intervals.get(i).start <= newInterval.end) {
+        newInterval = new Interval( // we could mutate newInterval here also
+                Math.min(newInterval.start, intervals.get(i).start),
+                Math.max(newInterval.end, intervals.get(i).end));
+        i++;
+    }
+
+
+
+45. Jump Game II 
+Given an array of non-negative integers, you are initially positioned at the first index of the array.
+
+Each element in the array represents your maximum jump length at that position.
+
+Your goal is to reach the last index in the minimum number of jumps.
+
+For example:
+Given array A = [2,3,1,1,4]
+
+The minimum number of jumps to reach the last index is 2. (Jump 1 step from index 0 to 1, then 3 steps to the last index.)
+
+记录每一步能到达的最远curmaxindex，然后超过该curmaxindex后算新的一步，在到达当前步能到的最远curmaxindex前，计算当前位置可到达的最远index，该index的max为超过curmaxindex后的新curmaxindex；
+
+
+
+123. Best Time to Buy and Sell Stock III
+Say you have an array for which the ith element is the price of a given stock on day i.
+
+Design an algorithm to find the maximum profit. You may complete at most two transactions.
+
+从左往右扫一遍作为第一次，从右往左扫一遍作为第二次，每个几点存以它为分界点时左右最大值(具体方法和|一样，可以通过记录min（或max对于从右向左）
+	min=Math.min(prices[i],min);
+    msofar=Math.max(msofar,prices[i]-min);
+    dp[i]+=msofar;
+），然后，找每个位置最大值
+
+at most k transactions:
+
+		for (int i = 1; i <= k; i++) {
+            int tmpMax =  -prices[0];
+            for (int j = 1; j < len; j++) {
+                t[i][j] = Math.max(t[i][j - 1], prices[j] + tmpMax);
+                tmpMax =  Math.max(tmpMax, t[i - 1][j - 1] - prices[j]);
+            }
+        }
+        tmpMax means the maximum profit of just doing at most i-1 transactions, using at most first j-1 prices, and buying the stock at price[j] - this is used for the next loop.
+
+
+
+
+
+
+99. Recover Binary Search Tree 
+Two elements of a binary search tree (BST) are swapped by mistake.
+
+Recover the tree without changing its structure.
+
+Note:
+A solution using O(n) space is pretty straight forward. Could you devise a constant space solution?
+
+思路很简单，就是找inorder中后面元素小与前一元素的两个地方，然后互换，为了实现O(1)空间，使用morris travesal方法：
+morris travesal方法： 即更改树结构来实现回溯
+	每次遍历左子树前，先进入左子树的最右元素（即遍历最后会遇到的元素），然后将其right child由null改为当前root，然后开始遍历左子树
+	遍历回root后改回null
+
+
+
+
+
+87. Scramble String
+Given a string s1, we may represent it as a binary tree by partitioning it to two non-empty substrings recursively.
+
+Below is one possible representation of s1 = "great":
+
+    great
+   /    \
+  gr    eat
+ / \    /  \
+g   r  e   at
+           / \
+          a   t
+To scramble the string, we may choose any non-leaf node and swap its two children.
+
+For example, if we choose the node "gr" and swap its two children, it produces a scrambled string "rgeat".
+
+    rgeat
+   /    \
+  rg    eat
+ / \    /  \
+r   g  e   at
+           / \
+          a   t
+We say that "rgeat" is a scrambled string of "great".
+
+Similarly, if we continue to swap the children of nodes "eat" and "at", it produces a scrambled string "rgtae".	
+
+
+
+递归判断是否anagram，是的话进行下一步判断，两种可能，
+1）在i位置分割，s1,s2前i子串和后len-i子串互为scramble。
+2）在i位置分隔，s1前i子串和s2后i子串，s1后len-i子串和s2前len-i子串互为scramble
+
+
+
+138. Copy List with Random Pointer 
+A linked list is given such that each node contains an additional random pointer which could point to any node in the list or null.
+
+Return a deep copy of the list.
+
+
+思路很简单，利用hashmap建立对应新老节点的关系，然后建遍历旧list的同时取元素建立新的list。同样的思路也可用于copy graph问题
+如果不适用hashmap结构，那么就把新node插入到对应老node后面，建完再改回来。实现hash功能
+
+
+
+
+
+
+164. Maximum Gap
+Given an unsorted array, find the maximum difference between the successive elements in its sorted form.
+
+Try to solve it in linear time/space.
+
+Return 0 if the array contains less than 2 elements.
+
+You may assume all elements in the array are non-negative integers and fit in the 32-bit signed integer range.
+
+
+
+O(n)的方法无外乎桶排序之类，此处因为求最大gap，所以只要把桶的长度定义小于最小可能gap长度，那么桶里面的元素gap
+可以不用管，只求桶之间的元素gap即可。此时记录每个桶最大最小元素即可，桶的index=（num[i]-min)/bucketgap
+
+
+
+
+
+
+
+315. Count of Smaller Numbers After Self 
+You are given an integer array nums and you have to return a new counts array. The counts array has the property where counts[i] is the number of smaller elements to the right of nums[i].
+
+Example:
+
+Given nums = [5, 2, 6, 1]
+
+To the right of 5 there are 2 smaller elements (2 and 1).
+To the right of 2 there is only 1 smaller element (1).
+To the right of 6 there is 1 smaller element (1).
+To the right of 1 there is 0 smaller element.
+Return the array [2, 1, 1, 0].
+
+
+从后向前扫到的数据加到一个sorted array中，每次二分查找插入位置，插入位置前元素的数量就是所求。
+
+
+
+
+72. Edit Distance 
+Given two words word1 and word2, find the minimum number of steps required to convert word1 to word2. (each operation is counted as 1 step.)
+
+You have the following 3 operations permitted on a word:
+
+a) Insert a character
+b) Delete a character
+c) Replace a character
+
+
+int[][] cost=new int[len1+1][len2+1];//注意，len+1的情况适用于一个元素也可以不要的时候。
+cost[i][j]表示前i子串编程前j子串所用最少操作数。
+cost[i+1][j+1]=cost[i][j] //当charAt(i)==charAt(j);
+cost[i+1][j+1]=1+min{replace, delete, insert} //otherwise; all for word1
+replace = cost[i][j];
+delete = cost[i][j+1];
+insert = cost[i+1][j];
+
+base case是cost[i][0]=cost[0][i]=i;
+
+
+
+
+
+
+25. Reverse Nodes in k-Group 
+Given a linked list, reverse the nodes of a linked list k at a time and return its modified list.
+
+If the number of nodes is not a multiple of k then left-out nodes in the end should remain as it is.
+
+You may not alter the values in the nodes, only nodes itself may be changed.
+
+Only constant memory is allowed.
+
+
+数出k个来反转，返回head，k个以后的元素递归调用得到后面的head
+
+
+
+
+
+115. Distinct Subsequences 
+Given a string S and a string T, count the number of distinct subsequences of T in S.
+
+A subsequence of a string is a new string which is formed from the original string by deleting some (can be none) of the characters without disturbing the relative positions of the remaining characters. (ie, "ACE" is a subsequence of "ABCDE" while "AEC" is not).
+
+Here is an example:
+S = "rabbbit", T = "rabbit"
+
+Return 3.
+
+
+dp题，真头大啊。
+	for(int i=0; i<T.length(); i++) {
+        for(int j=0; j<S.length(); j++) {
+            if(T.charAt(i) == S.charAt(j)) {
+                mem[i+1][j+1] = mem[i][j] + mem[i+1][j];
+            } else {
+                mem[i+1][j+1] = mem[i+1][j];
+            }
+        }
+    }
+base case 是，所有s子串都包含1个空串即mem[0][i]=1;
